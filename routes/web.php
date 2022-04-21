bf2127959bd7d8b22e34c1f1ce29b24f6d4c6e6b
Telvla <telvla.trifonov@gmail.com>
2022-04-21 23:26:19 +0300

    add upload

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
-
 Route::get('/', function () {
-
     //$key = base64_decode(md5(uniqid()));
     //dd($key);
     //Log::debug($key);
 
 });
 
-
-
 Route::get('/test-http', function (\App\Helpers\Telegram $telegram) {
     $telegram->sendMessage(835916638, '<b>Привет</b>');
-
 });
