de8ab33bdeff79c42f349e9c8e2b01fb5e669e6d
Telvla <telvla.trifonov@gmail.com>
2022-05-10 23:53:28 +0300

    upload project

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
+use App\Helpers\Telegram;
 
-Route::get('/start-message', function (\App\Helpers\Telegram $telegram) {
+Route::get('/start-message', function (Telegram $telegram) {
     $telegram->sendStartMessage(835916638);
 });
 
-Route::get('/img-prew', function (\App\Helpers\Telegram $telegram) {
+Route::get('/img-prew', function (Telegram $telegram) {
     $telegram->sendPrewImg(835916638);
 });
 
-Route::get('/test-http', function (\App\Helpers\Telegram $telegram) {
+Route::get('/test-http', function (Telegram $telegram) {
     $telegram->sendMessage(835916638, 'Send message');
 });
