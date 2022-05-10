b4b8de76fa2258604fae33ca876c2f69ad7ee2f8
Telvla <telvla.trifonov@gmail.com>
2022-05-10 23:45:10 +0300

    upload project

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
-
-Route::get('/', function () {
-    //$key = base64_decode(md5(uniqid()));
-    //Log::debug($key);
-});
 
 Route::get('/start-message', function (\App\Helpers\Telegram $telegram) {
     $telegram->sendStartMessage(835916638);
